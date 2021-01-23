640db7e1b6cb9f400eec877db51d75d595e3954c
Alexey Gavrilov <54736886+eeealesha@users.noreply.github.com>
2021-01-23 22:23:33 +0300

    undate
    Делал в mesto-express

---
+++ helpers/files.js
@@ -0,0 +1,8 @@
+const fsPromises = require('fs').promises;
+
+const getDataFromFile = (pathToFile) => {
+  return fsPromises.readFile(pathToFile, { encoding: 'utf-8' })
+    .then(data => JSON.parse(data))
+};
+
+module.exports = getDataFromFile;
